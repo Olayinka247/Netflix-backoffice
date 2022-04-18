e3e6028f33868128eddd099cdc956fe4fb24a9c4
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-04-18 19:00:20 +0100

    GET

--- backoffice.js
+++ backoffice.js
@@ -1,8 +1,29 @@
 const endpoint = "https://striveschool-api.herokuapp.com/api/movies";
 
+const addMovies = {
+  name: "",
+  description: "",
+  category: "",
+  imageUrl: "",
+};
+
+function createdMovies() {
+  addMovies.name = document.getElementById("movie-name").value;
+  addMovies.description = document.getElementById("movie-description").value;
+  addMovies.category = document.getElementById("movie-category").value;
+  addMovies.imageUrl = document.getElementById("movie-imageUrl").value;
+}
+
+function submitMovies(event) {
+  event.preventDefault();
+  createdMovies();
+  makeMovies();
+}
+
 async function makeMovies() {
   const response = await fetch(endpoint, {
     headers: {
+      "content-Type": "application/json",
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjI4YjI0MzRlYTdiMTAwMTVkMDY3YWYiLCJpYXQiOjE2NTAyOTY3MTcsImV4cCI6MTY1MTUwNjMxN30.K9txPJF10Wv5lMPjkpXlSdmlT8Y_1aUlPBZl-XQGxZw",
     },
@@ -15,17 +36,3 @@ async function makeMovies() {
   }
 }
 makeMovies();
-
-const addMovies = {
-  name: "",
-  description: "",
-  category: "",
-  imageUrl: "",
-};
-
-function createdMovies() {
-  addMovies.name = document.getElementById("product-name").value;
-  addMovies.description = document.getElementById("product-description").value;
-  addMovies.category = document.getElementById("product-brand").value;
-  addMovies.imageUrl = document.getElementById("product-imageUrl").value;
-}
--- netflix.js
+++ netflix.js
@@ -1,7 +1,3 @@
-window.onload = () => {
-  listMovies();
-};
-
 const endpoint = "https://striveschool-api.herokuapp.com/api/movies";
 
 async function listMovies() {
@@ -14,6 +10,27 @@ async function listMovies() {
   if (response.ok) {
     const data = await response.json();
     console.log(data);
+    const row1 = document.querySelector(".row");
+    row1.innerHTML = data
+      .map(
+        (movies) => `
+    <div class="col-2 ">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="${movies.imgUrl}" class="w-100 rounded" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="${movies.imgUrl}" class=" w-100 rounded" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="${movies.imgUrl}" class=" w-100 rounded" alt="...">
+        </div>
+      </div>
+    </div>
+  </div>`
+      )
+      .join("");
   }
 }
 listMovies();
